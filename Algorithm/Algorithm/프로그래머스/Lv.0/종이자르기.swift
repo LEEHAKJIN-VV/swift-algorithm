3dbb31e78663429008e260b7e4e910e3b33647ed
LEEHAKJIN-VV <52197436+LEEHAKJIN-VV@users.noreply.github.com>
2022-12-12 19:43:02 +0900

    Lv0 종이자르기 추가
    Lv0 종이자르기 추가

---
+++ Algorithm/Algorithm/프로그래머스/Lv.0/종이자르기.swift
@@ -0,0 +1,13 @@
+//
+//  종이자르기.swift
+//  Algorithm
+//
+//  Created by 이학진 on 2022/12/12.
+//
+
+import Foundation
+
+func solution(_ M:Int, _ N:Int) -> Int {
+    return M - 1 + ((N - 1) * M)
+}
+
